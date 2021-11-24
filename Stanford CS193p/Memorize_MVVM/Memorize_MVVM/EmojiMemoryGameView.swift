54844fb4ede10bd35d286e140d8c83331d60d4ab
kwsong0113 <kwsong0113@gmail.com>
2021-11-24 06:42:50 -0800

    Draw pie timer on Memorize cards

--- Stanford CS193p/Memorize_MVVM/Memorize_MVVM/EmojiMemoryGameView.swift
+++ Stanford CS193p/Memorize_MVVM/Memorize_MVVM/EmojiMemoryGameView.swift
@@ -28,9 +28,9 @@ struct EmojiMemoryGameView: View {
                         }
                     }
                 }
-                .padding(.horizontal)
-            Text(game.themeName)
+                Text(game.themeName)
             }
+            .padding([.horizontal, .top])
             .foregroundColor(.mint)
             .font(.largeTitle)
             AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
@@ -57,8 +57,9 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Text(card.content)
-                        .font(font(in: geometry.size))
+                    Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 110 - 90))
+                        .padding(5).opacity(0.5)
+                    Text(card.content).font(font(in: geometry.size))
                 } else if card.isMatched {
                     shape.opacity(0)
                 } else {
@@ -75,7 +76,7 @@ struct CardView: View {
     private struct DrawingConstants {
         static let cornerRadius: CGFloat = 15
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.75
+        static let fontScale: CGFloat = 0.65
     }
 }
 
@@ -97,9 +98,7 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
-        EmojiMemoryGameView(game: game)
-            .preferredColorScheme(.dark)
-        EmojiMemoryGameView(game: game)
-            .preferredColorScheme(.light)
+        game.choose(game.cards.first!)
+        return EmojiMemoryGameView(game: game)
     }
 }
---
+++ Stanford CS193p/Memorize_MVVM/Memorize_MVVM/Pie.swift
@@ -0,0 +1,37 @@
+//
+//  Pie.swift
+//  Memorize_MVVM
+//
+//  Created by user206692 on 11/24/21.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool = false
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * CGFloat(cos(startAngle.radians)),
+            y: center.y + radius * CGFloat(sin(endAngle.radians))
+        )
+        
+        var p = Path()
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(center: center,
+                 radius: radius,
+                 startAngle: startAngle,
+                 endAngle: endAngle,
+                 clockwise: !clockwise)
+        p.addLine(to: center)
+        return p
+    }
+    
+    
+}
