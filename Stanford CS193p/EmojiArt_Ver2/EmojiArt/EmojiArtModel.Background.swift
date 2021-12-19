ad3acf6f5cc1a219ceedd0de7e45feb523ab586b
Kiwhan Song <kwsong0113@gmail.com>
2021-12-19 02:22:44 -0800

    Copy the version of EmojiArt from lecture 10

---
+++ Stanford CS193p/EmojiArt_Ver2/EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiArtModel.Background.swift
+//  EmojiArt
+//
+//  Created by user211007 on 12/12/21.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    enum Background: Equatable {
+        case blank
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case .url(let url): return url
+            default: return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data): return data
+            default: return nil
+            }
+        }
+    }
+}
