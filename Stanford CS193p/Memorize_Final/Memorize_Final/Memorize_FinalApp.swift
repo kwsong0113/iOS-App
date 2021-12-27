8da00ea36480f7c4dfb23a9086df8f8b5de284d4
Kiwhan Song <kwsong0113@gmail.com>
2021-12-27 05:41:52 -0800

    Start developing final Memorize app

---
+++ Stanford CS193p/Memorize_Final/Memorize_Final/Memorize_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_FinalApp.swift
+//  Memorize_Final
+//
+//  Created by user211007 on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
