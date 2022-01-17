0844c3f86bb23065da018f685e3ae908bfa9a09c
Kiwhan Song <kwsong0113@gmail.com>
2022-01-17 05:32:34 -0800

    Copy emojiArt app from Lecture 12

---
+++ Stanford CS193p/EmojiArt_Ver4/EmojiArt_Ver4/EmojiArt_Ver4App.swift
@@ -0,0 +1,22 @@
+//
+//  EmojiArt_Ver4App.swift
+//  EmojiArt_Ver4
+//
+//  Created by user211007 on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArt_Ver4App: App {
+    @StateObject var document = EmojiArtDocument()
+    @StateObject var paletteStore = PaletteStore(named: "Default")
+    
+    var body: some Scene {
+        WindowGroup {
+            EmojiArtDocumentView(document: document)
+                // L12 "inject" our PaletteStore ViewModel into our View hierarchy
+                .environmentObject(paletteStore)
+        }
+    }
+}
